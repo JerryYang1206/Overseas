d4e121d972693228a41f5ad9b245cc1b240bf041
wl <wangle1109@outlook.com>
2018-06-29 11:25:38 +0800

    s

--- app/src/main/java/com/overseas/overseas/im/RongChatActivity.java
+++ app/src/main/java/com/overseas/overseas/im/RongChatActivity.java
@@ -1,38 +1,17 @@
 package com.overseas.overseas.im;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.text.TextUtils;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.CheckBox;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
 import com.lzy.okgo.model.Response;
-import com.overseas.overseas.MyApplication;
 import com.overseas.overseas.R;
 import com.overseas.overseas.base.BaseActivity;
-import com.overseas.overseas.base.BaseDialog;
 import com.overseas.overseas.model.UserBean;
 import com.overseas.overseas.presenter.FromPhonePresenter;
-import com.overseas.overseas.utils.CacheUtils;
-import com.overseas.overseas.utils.Constants;
-import com.overseas.overseas.utils.SharedPreferencesUtils;
-import com.overseas.overseas.view.CircleImageView;
-import com.overseas.overseas.view.RatingBarView;
-
-
-import java.util.List;
-
-import io.rong.imkit.DefaultExtensionModule;
-import io.rong.imkit.IExtensionModule;
-import io.rong.imkit.RongExtensionManager;
 import io.rong.imkit.RongIM;
 import io.rong.imlib.model.Message;
 
@@ -52,8 +31,6 @@ public class RongChatActivity extends BaseActivity implements FromPhonePresenter
 
     private int state = 0; //0否 1是
 
-    private String avatar = "";
-
     private String name;
 
     @Override
@@ -67,11 +44,16 @@ public class RongChatActivity extends BaseActivity implements FromPhonePresenter
 
         //会话界面 对方id
         targetId = getIntent().getData().getQueryParameter("targetId");
+
         presenter = new FromPhonePresenter(this, this);
         presenter.getUserPhone(targetId);
 
         //对方 昵称
         name = getIntent().getData().getQueryParameter("title");
+
+        if (name != null) {
+            title.setText(name);
+        }
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -93,21 +75,22 @@ public class RongChatActivity extends BaseActivity implements FromPhonePresenter
             }
         });
 
-//        RongIM.getInstance().setSendMessageListener(new RongIM.OnSendMessageListener() {
-//            @Override
-//            public Message onSend(Message message) {
-//                /**
-//                 * getTargetId   接受者id
-//                 * 如果不使用的话将此方法删除
-//                 */
-//                return message;
-//            }
-//
-//            @Override
-//            public boolean onSent(Message message, RongIM.SentMessageErrorCode sentMessageErrorCode) {
-//                return false;
-//            }
-//        });
+        RongIM.getInstance().setSendMessageListener(new RongIM.OnSendMessageListener() {
+            @Override
+            public Message onSend(Message message) {
+                /**
+                 * getTargetId   接受者id
+                 * 如果不使用的话将此方法删除
+                 */
+                presenter.recordReply(targetId);
+                return message;
+            }
+
+            @Override
+            public boolean onSent(Message message, RongIM.SentMessageErrorCode sentMessageErrorCode) {
+                return false;
+            }
+        });
     }
 
     @Override
